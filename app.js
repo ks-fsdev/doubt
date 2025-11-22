7e48872d28ed0e967930c04eec5ad32f1f627655
Khushi Srivastava <sweetkhushi2000@gmail.com>
2025-11-22 17:37:13 +0530

    dw

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
-// // 1. when I set item in sessionStorage, it should have concatanated but it didn't... it just added like normal. i didn't had the chance to use parsInt like you told in the video
-
-// let count = 0;
-
-// incBtn.addEventListener("click", () => {
-//   console.log("inc clicked");
-//   count = count + 1;
-//   console.log(count);
-//   sessionStorage.setItem("count", count);
-
-// });
-
-// 2. how do we make the calender stuff on airbnb listing page
-
-// // 3. the localStorage and session storage are not working as expected
+// // 1. the localStorage and session storage are not working as expected
 
 // const counterDisplay = document.getElementById("counter");
 // const incBtn = document.getElementById("increase");
@@ -86,36 +72,9 @@
 //   localStorage.setItem("isDark", isDark);
 // }
 
-// //4. now what to do. i can see the qoutes inside the array but how to fetch it
+//2. tourist app seasonal doubt
 
-// fetch("https://dummyjson.com/quotes").then((data) => {
-//   console.log(data.json());
-// });
-
-// // when i do this just like mentioned on the website then and only then it works
-// fetch("https://dummyjson.com/quotes")
-//   .then((res) => res.json())
-//   .then(console.log);
-
-// // this is not working with my api key
-// async function fetchQuotes() {
-//   let data = await fetch("https://dummyjson.com/quotes");
-//   let quotes = data.json();
-//   console.log(quotes);
-// }
-// fetchQuotes();
-
-// 5. my todo app is showing less functioning than designed. it works well  in my desktop  but enter function and dragger is not working when desployed
-
-// // 6. why isn't anything rendering without this line in app.js
-// import { useState } from "react";
-// // when in videos you are able to render it without importing anything.
-
-//tourist app seasonal doubt
-
-// 7. react js instagram proj : the id from timeline doeswn't open at the top of the page
-
-// // 8. custom classes and root in tailwind css
+// // 3. custom classes and root in tailwind css
 // import @tailwindcss
 
 // @tailwind base;
@@ -136,23 +95,8 @@
 //   }
 // }
 
-// // 9. found one of the svgs which crops instead of decreasing in size when changing the width, & height
+//4. dribble navbar
 
-// <svg
-//   xmlns="http://www.w3.org/2000/svg"
-//   width="24"
-//   height="24"
-//   fill="none"
-//   aria-hidden="true"
-//   className="icon"
-// >
-//   <path
-//     stroke="currentColor"
-//     stroke-linecap="round"
-//     stroke-linejoin="round"
-//     stroke-width="2"
-//     d="m22 7-7.869 7.869c-.396.396-.594.594-.822.668a1 1 0 0 1-.618 0c-.228-.074-.426-.272-.822-.668L9.13 12.13c-.396-.396-.594-.594-.822-.668a1 1 0 0 0-.618 0c-.228.074-.426.272-.822.668L2 17M22 7h-7m7 0v7"
-//   />
-// </svg>;
+// 5. chatgpt clone api key no working
 
-// 10. i do not find tailwind.config.js in my project when i install  tailwind... and i wanted to use custom classes using utility and root as directed in the documentation it's not wqorking
+// 6. instagram clone global updation of request acceptance. how to do it??
