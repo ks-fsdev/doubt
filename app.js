dcd6519711d4fad6d767321fbd75c75d5afe29cd
Khushi Srivastava <sweetkhushi2000@gmail.com>
2025-11-23 19:52:27 +0530

    fs

--- app.js
+++ app.js
@@ -1,79 +1,3 @@
-// // 1. the localStorage and session storage are not working as expected
-
-// const counterDisplay = document.getElementById("counter");
-// const incBtn = document.getElementById("increase");
-// const decBtn = document.getElementById("decrease");
-
-// let count = 0;
-// renderCount();
-
-// incBtn.addEventListener("click", () => {
-//   count = count + 1;
-//   console.log(count);
-//   renderCount();
-// });
-
-// decBtn.addEventListener("click", () => {
-//   count = count - 1;
-//   console.log(count);
-//   renderCount();
-// });
-
-// function renderCount() {
-//   sessionStorage.setItem("count", count);
-//   counterDisplay.innerText = sessionStorage.getItem("count");
-// }
-
-// //attempting theme changing using localSession
-// const themeBtn = document.getElementById("themeBtn");
-// const body = document.getElementById("body");
-// const counterSec = document.getElementById("counterSec");
-
-// let isDark = false;
-// updateLocalSession();
-
-// themeBtn.addEventListener("click", () => {
-//   if (isDark === false) {
-//     isDark = true;
-//     console.log("is dark " + isDark);
-//     changeTheme();
-//     updateLocalSession();
-//   } else {
-//     isDark = false;
-//     console.log("is dark " + isDark);
-//     changeTheme();
-//     updateLocalSession();
-//   }
-// });
-
-// function changeTheme() {
-//   if (isDark === true) {
-//     body.classList.add("bg-black");
-//     counterSec.classList.add("text-white", "border-white");
-//     incBtn.classList.add("bg-white", "text-black", "font-bold");
-//     decBtn.classList.add("bg-white", "text-black", "font-bold");
-//     incBtn.classList.remove("bg-black", "text-white");
-//     decBtn.classList.remove("bg-black", "text-white");
-//     themeBtn.classList.remove("bg-black", "text-white");
-//     themeBtn.classList.add("bg-white", "text-black", "font-bold");
-//   } else {
-//     body.classList.remove("bg-black");
-//     counterSec.classList.remove("text-white", "border-white");
-//     incBtn.classList.remove("bg-white", "text-black", "font-bold");
-//     decBtn.classList.remove("bg-white", "text-black", "font-bold");
-//     incBtn.classList.add("bg-black", "text-white");
-//     decBtn.classList.add("bg-black", "text-white");
-//     themeBtn.classList.add("bg-black", "text-white");
-//     themeBtn.classList.remove("bg-white", "text-black", "font-bold");
-//   }
-// }
-
-// function updateLocalSession() {
-//   localStorage.setItem("isDark", isDark);
-// }
-
-//2. tourist app seasonal doubt
-
 // // 3. custom classes and root in tailwind css
 // import @tailwindcss
 
@@ -95,8 +19,6 @@
 //   }
 // }
 
-//4. dribble navbar
-
-// 5. chatgpt clone api key no working
+//4. dribble navbar & dribble magic ui button that is uploaded on git hub but during deployment it says vercel cannot find it while deploying.
 
 // 6. instagram clone global updation of request acceptance. how to do it??
